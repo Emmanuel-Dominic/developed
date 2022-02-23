c28a050884b6bdc04802c6950005d5dd2cd09bae
Matembu Emmanuel Dominic <ematembu2@gmail.com>
2022-02-23 10:19:41 +0300

    add template files

--- config/settings.py
+++ config/settings.py
@@ -68,7 +68,7 @@ LOGIN_URL = os.getenv('LOGIN_URL')
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
